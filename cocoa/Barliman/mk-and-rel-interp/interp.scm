5d56bac541fa1c764505648bac212b16348279a7
Will Byrd <webyrd@gmail.com>
2016-06-14 15:14:52 -0600

    added the new parser code to the main interpreter file

--- cocoa/Barliman/mk-and-rel-interp/interp.scm
+++ cocoa/Barliman/mk-and-rel-interp/interp.scm
@@ -1,3 +1,218 @@
+;; parser
+
+;; see Barliman/interpreter_experiments/parse.scm and
+;; Barliman/interpreter_experiments/parse-tests.scm
+;; for more on the parser.
+(define (parseo expr)
+  (parse-expo expr initial-env))
+
+(define (parse-expo expr env)
+  (conde
+    ((fresh (val)
+       (== `(quote ,val) expr)
+       (absento 'closure val)
+       (absento 'prim val)
+       (not-in-envo 'quote env)))
+
+    ((numbero expr))
+
+    ((symbolo expr)
+     (fresh (val)
+       ;;
+       (conde
+         ((keywordo expr)
+          ;; if expr is a keyword ('lambda', 'begin', 'define', 'letrec', 'and', 'or', 'if', 'match', or whatever)
+          ;; *and* expr is shadowed in the environment, succeed.  Otherwuse, it is a syntax violation.
+          (fresh (val)
+            (lookupo expr env val)))
+         ((not-keywordo expr)
+          ;; Otherwise, succeed, since it doesn't matter if the symbol
+          ;; is bound or not -- the expression is still grammatically
+          ;; correct.
+          ))))
+
+    ((fresh (x body)
+       (== `(lambda ,x ,body) expr)
+       (not-in-envo 'lambda env)
+       (conde
+         ;; Variadic
+         ((symbolo x)
+          (fresh (val env^)
+            (== `((,x . (val . ,val)) . ,env) env^)
+            (parse-expo body env^)))
+         ;; Multi-argument
+         ((list-of-symbolso x)
+          (fresh (vals env^)
+            (ext-env*o x vals env env^)
+            (parse-expo body env^))))))
+
+    ((fresh (defn args name body e)
+       (== `(begin ,defn ,e) expr)
+       (== `(define ,name (lambda ,args ,body)) defn)
+       (parse-expo `(letrec ((,name (lambda ,args ,body))) ,e) env)))
+
+    ((fresh (rator rands)
+       (== `(,rator . ,rands) expr)
+       ;; application
+       (parse-listo `(,rator . ,rands) env)))
+
+    ((parse-matcho expr env))
+
+    ((fresh (p-name x body letrec-body)
+       ;; single-function variadic letrec version
+       (== `(letrec ((,p-name (lambda ,x ,body)))
+              ,letrec-body)
+           expr)
+       (not-in-envo 'letrec env)
+       (conde
+         ; Variadic
+         ((symbolo x)
+          (fresh (val env^)
+            (== `((,x . (val . ,val)) . ,env) env^)
+            (parse-expo body env^)))
+         ; Multiple argument
+         ((list-of-symbolso x)
+          (fresh (vals env^)
+            (ext-env*o x vals env env^)
+            (parse-expo body env^))))
+       (parse-expo letrec-body
+                     `((,p-name . (rec . (lambda ,x ,body))) . ,env))))
+
+    ((prim-parseo expr env))
+    
+    ))
+
+(define (parse-listo expr env)
+  (conde
+    ((== '() expr))
+    ((fresh (a d)
+       (== `(,a . ,d) expr)
+       (parse-expo a env)
+       (parse-listo d env)))))
+
+(define (prim-parseo expr env)
+  (conde
+    ((boolean-parseo expr env))
+    ((and-parseo expr env))
+    ((or-parseo expr env))
+    ((if-parseo expr env))))
+
+(define (boolean-parseo expr env)
+  (conde
+    ((== #t expr))
+    ((== #f expr))))
+
+(define (and-parseo expr env)
+  (fresh (e*)
+    (== `(and . ,e*) expr)
+    (not-in-envo 'and env)
+    (parse-listo e* env)))
+
+(define (or-parseo expr env)
+  (fresh (e*)
+    (== `(or . ,e*) expr)
+    (not-in-envo 'or env)
+    (parse-listo e* env)))
+
+(define (if-parseo expr env)
+  (fresh (e1 e2 e3)
+    (== `(if ,e1 ,e2 ,e3) expr)
+    (not-in-envo 'if env)
+    (parse-expo e1 env)
+    (parse-expo e2 env)
+    (parse-expo e3 env)))
+
+(define keywordo
+  (lambda (x)
+    (conde
+      ((== 'lambda x))
+      ((== 'begin x))
+      ((== 'define x))
+      ((== 'letrec x))
+      ((== 'and x))
+      ((== 'or x))
+      ((== 'if x))
+      ((== 'match x))
+      ((== 'quasiquote x))
+      ((== 'unquote x)))))
+
+(define not-keywordo
+  (lambda (x)
+    (fresh ()
+      (symbolo x)
+      (=/= 'lambda x)
+      (=/= 'begin x)
+      (=/= 'define x)
+      (=/= 'letrec x)
+      (=/= 'and x)
+      (=/= 'or x)
+      (=/= 'if x)
+      (=/= 'match x)
+      (=/= 'quasiquote x)
+      (=/= 'unquote x))))
+
+
+;;; handle 'match'
+
+(define parse-matcho
+  (lambda  (expr env)
+    (fresh (against-expr clause clauses)
+      (== `(match ,against-expr ,clause . ,clauses) expr)
+      (not-in-envo 'match env)
+      (parse-expo against-expr env)
+      (parse-clauses `(,clause . ,clauses) env))))
+
+(define (parse-clauses clauses env)
+  (conde
+    ((== '() clauses))
+    ((fresh (pat result-expr d)
+       (== `((,pat ,result-expr) . ,d) clauses)
+       (fresh (penv env^)
+         (parse-pattern pat '() penv)
+         (regular-env-appendo penv env env^)
+         (parse-expo result-expr env^))
+       (parse-clauses d env)))))
+
+(define (parse-pattern pat penv penv-out)
+  (conde
+    ((self-eval-literalo pat)
+     (== penv penv-out))
+    ((parse-var-pat-match pat penv penv-out))
+    ((fresh (var pred)
+      (== `(? ,pred ,var) pat)
+      (conde
+        ((== 'symbol? pred))
+        ((== 'number? pred)))
+      (parse-var-pat-match var penv penv-out)))
+    ((fresh (quasi-pat)
+      (== (list 'quasiquote quasi-pat) pat)
+      (parse-quasiquote-pattern quasi-pat penv penv-out)))))
+
+(define (parse-var-pat-match var penv penv-out)
+  (fresh (_)
+    (symbolo var)
+    (conde
+      ((== penv penv-out)
+       (lookupo var penv _))
+      ((== `((,var . (val . ,_)) . ,penv) penv-out)
+       (not-in-envo var penv)))))
+
+(define (parse-quasiquote-pattern quasi-pat penv penv-out)
+  (conde
+    ((== penv penv-out)
+     (literalo quasi-pat))
+    ((fresh (pat)
+      (== (list 'unquote pat) quasi-pat)
+      (parse-pattern pat penv penv-out)))
+    ((fresh (a d penv^)
+       (== `(,a . ,d) quasi-pat)
+       (=/= 'unquote a)
+       (parse-quasiquote-pattern a penv penv^)
+       (parse-quasiquote-pattern d penv^ penv-out)))))
+
+
+
+;; interpreter
 (define (evalo expr val)
   (eval-expo expr initial-env val))
 
