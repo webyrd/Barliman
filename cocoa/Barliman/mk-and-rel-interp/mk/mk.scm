5af6fca8e1d9b215ecb9c337d3d4218cdf02b06c
gregr <greg.weiqi@gmail.com>
2016-10-16 09:39:25 -0400

    Define weighted versions of conde[$]

--- cocoa/Barliman/mk-and-rel-interp/mk/mk.scm
+++ cocoa/Barliman/mk-and-rel-interp/mk/mk.scm
@@ -406,6 +406,14 @@
                   (lambdag@ (st)
                     (mplus*-dfs (bind*-depth st g0 g ...)
                                 (bind*-depth st g1 g^ ...) ...))))))))
+(define-syntax conde-weighted
+  (syntax-rules ()
+    ((_ (w0 g0 g ...) (w1 g1 g^ ...) ...)
+     (lambdag@ (st)
+       (inc (bind (state-depth-deepen (state-with-scope st (new-scope)))
+                  (lambdag@ (st)
+                    (mplus*-weighted (w0 (bind*-depth st g0 g ...))
+                                     (w1 (bind*-depth st g1 g^ ...)) ...))))))))
 
 (define-syntax conde$
   (syntax-rules ()
@@ -423,6 +431,14 @@
          (mplus*-dfs
            (bind*-depth st g0 g ...)
            (bind*-depth st g1 g^ ...) ...))))))
+(define-syntax conde$-weighted
+  (syntax-rules ()
+    ((_ (w0 g0 g ...) (w1 g1 g^ ...) ...)
+     (lambdag@ (st)
+       (let ((st (state-with-scope st (new-scope))))
+         (mplus*-weighted
+           (w0 (bind*-depth st g0 g ...))
+           (w1 (bind*-depth st g1 g^ ...)) ...))))))
 
 (define-syntax case0
   (syntax-rules ()
@@ -480,6 +496,11 @@
     ((_ e) e)
     ((_ e0 e ...) (mplus-dfs e0
                     (inc (mplus*-dfs e ...))))))
+(define-syntax mplus*-weighted
+  (syntax-rules ()
+    ((_ (_ e)) e)
+    ((_ (w0 e0) e ...) (mplus-weighted
+                         w0 w0 1 e0 (inc (mplus*-weighted e ...))))))
 
 (define mplus
   (lambda (c-inf f)
@@ -496,6 +517,20 @@
       ((f^) (inc (mplus-dfs (f^) f)))
       ((c) (choice c f))
       ((c f^) (choice c (inc (mplus-dfs (f^) f)))))))
+
+(define (mplus-weighted-next remaining weight other f^ f)
+  (inc (let ((weight-next (and remaining (- remaining 1))))
+         (if (and weight-next (<= weight-next 0))
+           (mplus-weighted other other weight (f) f^)
+           (mplus-weighted weight-next weight other (f^) f)))))
+(define mplus-weighted
+  (lambda (remaining weight other-weight c-inf f)
+    (case-inf c-inf
+      (() (f))
+      ((f^) (inc (mplus-weighted-next remaining weight other-weight f^ f)))
+      ((c) (choice c f))
+      ((c f^) (choice c (inc (mplus-weighted-next
+                               remaining weight other-weight f^ f)))))))
 
 
 ; Constraints
