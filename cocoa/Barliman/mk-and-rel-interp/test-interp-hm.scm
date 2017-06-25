b149ae0f1db7a28a1cc7f692826e511b857945a4
gregr <greg.weiqi@gmail.com>
2017-06-25 03:18:24 -0400

    Support pairs and nil

--- cocoa/Barliman/mk-and-rel-interp/test-interp-hm.scm
+++ cocoa/Barliman/mk-and-rel-interp/test-interp-hm.scm
@@ -33,3 +33,19 @@
   (run* (type)
     (:o '(letrec ((id (lambda (x) x))) (id id)) type))
   '(((-> _.0 _.0))))
+
+(test 'data-1
+  (run* (type) (:o ''(1 . (x #t #f)) type))
+  '(((cons num (cons sym (cons bool (cons bool ())))))))
+
+(test 'data-2
+  (run* (type) (:o '(cons 1 (cons 'x (cons #t (cons #f '())))) type))
+  '(((cons num (cons sym (cons bool (cons bool ())))))))
+
+(test 'data-3
+  (run* (type) (:o '(car (cons 1 (cons 'x (cons #t (cons #f '()))))) type))
+  '((num)))
+
+(test 'data-4
+  (run* (type) (:o '(cdr (cons 1 (cons 'x (cons #t (cons #f '()))))) type))
+  '(((cons sym (cons bool (cons bool ()))))))
