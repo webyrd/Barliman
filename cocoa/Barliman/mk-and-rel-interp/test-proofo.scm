cf4b8870f30ab2f233c56e57ee62112486b5dd7c
Jason Hemann <jason.hemann@gmail.com>
2025-03-23 00:19:23 -0400

    Adding a proofo 2 example to the barliman tests

--- cocoa/Barliman/mk-and-rel-interp/test-proofo.scm
+++ cocoa/Barliman/mk-and-rel-interp/test-proofo.scm
@@ -51,3 +51,61 @@
                   (A (A (A => B) (B => C)) assumption ()))))))))
  
  )
+
+
+(time
+  (test "proofo 2"
+     (let ()
+       (define (ans-allTests)
+         (define (results)
+           (run 1 (prf)
+             (let ()
+               (fresh
+                (body)
+                (== prf `(((A => B) => ((B => C) => (A => C))) () . ,body))
+                (evalo
+                 `(letrec ([member?
+                            (lambda (x ls)
+                              (if (null? ls) #f
+                                  (if (equal? (car ls) x) #t
+                                      (member? x (cdr ls)))))])
+                    (letrec ([proof?
+                              (lambda (proof)
+                                (match proof
+                                  [`(,A ,assms assumption ()) (member? A assms)]
+                                  [`(,B ,assms modus-ponens
+                                        (((,A => ,B) ,assms ,r1 ,ants1)
+                                         (,A ,assms ,r2 ,ants2)))
+                                   (and (proof? (list (list A '=> B) assms r1 ants1))
+                                        (proof? (list A assms r2 ants2)))]
+                                  [`((,A => ,B) ,assms conditional
+                                     ((,B (,A . ,assms) ,rule ,ants)))
+                                   (proof? (list B (cons A assms) rule ants))]
+                                  ))])
+                      (proof? ',prf)))
+                 #t)))))
+         (let ((results-fast (begin (set! allow-incomplete-search? #t) (results))))
+           (if (null? results-fast)
+               (begin (set! allow-incomplete-search? #f) (results))
+               results-fast)))
+       (ans-allTests))
+
+     ;; result!
+'(((((A => B) => ((B => C) => (A => C)))
+       ()
+       conditional
+       ((((B => C) => (A => C))
+         ((A => B))
+         conditional
+         (((A => C)
+           ((B => C) (A => B))
+           conditional
+           ((C (A (B => C) (A => B))
+               modus-ponens
+               (((B => C) (A (B => C) (A => B)) assumption ())
+                (B (A (B => C) (A => B))
+                   modus-ponens
+                   (((A => B) (A (B => C) (A => B)) assumption ())
+                    (A (A (B => C) (A => B)) assumption ()))))))))))))))
+
+ )
